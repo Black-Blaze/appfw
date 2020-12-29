08c8017c87f89f87965baf8539c8ce8585164a07
Black-Blaze <76452142+Black-Blaze@users.noreply.github.com>
2020-12-29 16:07:33 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,84 +1,14 @@
-import os, sys, re
-from BBWebFw import backend
+from BBWebFw import BBlazeWeb
+from BBWebFw.FileRenderer import FileRenderer
 
-class webApp:
-    def __init__(self, name, server):
-        self.urls = {}
-        self.server = server
-        self.name = name
-        self.error = {"urlcatcherexists" : "ERR:URL_CATCHER_ALREADY_EXISTS"}
-        backend.api(name, server)
+app =  BBlazeWeb.webApp("__init__", "GUNICORN")
 
-    def __call__(self, environ, start_response):
-        return backend.api.__call__(self, environ, start_response)
-    
-    def catchURL(self, path):
-        return backend.api.catchURL(self, path)
+@app.catchURL('/hello') #Goto host/hello
+def hello(response):
+       response.text = "Hello world!"
 
+@app.catchURL('/err404') #Goto host/err404
+def err404(response):
+       response.status_code = 404
 
-    def run(self, app, debug=False):
-        backend.api.run(self, app)
-
-    def handle_request(self, request):
-        return backend.api.handle_request(self, request)
-    
-    def find_handler(self, request):
-        return backend.api.find_handler(self, request)
-
-    def err404(self,response):
-        return backend.api.err404(self, response)
-
-    def setError(self, code, data):
-        return backend.api.setError(self, code, data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-def run(self, app, debug=False):
-        print("run")
-        host = "127.0.0.1"
-        port = 6000
-
-        if (self.name).endswith(".py"):
-            self.fname = self.name
-            self.name = (self.name).replace(".py", "")
-        else:
-            self.fname = self.name + ".py"
-        
-        sys.argv = [re.sub(r'(-script\.pyw|\.exe)?$', '', "env/bin/gunicorn"),self.name+':'+ app]
-        print(sys.argv)
-        if debug: 
-            with open(self.fname) as f:
-                self.old = f.read()
-                while True:
-                    print("11")
-                    with open(self.fname) as f:
-                        if f.read() == self.old:
-                            print("12")
-                            self.old = f.read()
-                            print("changed!!!")
-
-                        time.sleep(5)
-        else:
-            backend.run()
-        
-'''
+app.setError(404, FileRenderer.template("error.html", {"error": 404}))
